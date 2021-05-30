0aaa58ed4b93878221b5fafdc5db4505ac86b1e1
Akash9409 <64356858+Akash9409@users.noreply.github.com>
2021-05-30 21:49:00 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import os
+import json
+import requests
+
+client = discord.Client()
+
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+
+  # if message.content.startswith('$hello'):
+  #       await message.channel.send('Hello!')
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+  if message.content.startswith('$hello'):
+    msg = 'Kya haal hai {0.author.mention}'.format(message)
+    await message.channel.send(msg)
+
+
+client.run(os.environ['DISCORD_TOKEN'])
